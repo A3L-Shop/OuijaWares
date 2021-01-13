2c74b1fbfd65d0079671868258f788caf6474d25
Ali Wojcikiewicz <aliwoj@Alis-MBP.cable.rcn.com>
2021-01-13 16:14:54 -0600

    test: test for cart redux store, WIP test for cart react component

--- client/components/Cart.spec.js
+++ client/components/Cart.spec.js
@@ -2,20 +2,23 @@ import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {Cart} from './Cart'
+import Cart from './Cart'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-describe('UserHome', () => {
+describe('Cart', () => {
   let userCart
 
   beforeEach(() => {
-    const contents = [{product: {id: 1, name: 'mirror'}, amount: 2}]
-    userCart = shallow(<Cart cartItems={contents} />)
+    // const contents = [
+    //   {product: {id: 1, name: 'mirror', price: 5.55}, amount: 2},
+    //   {product: {id: 2, name: 'picture', price: 10.65}, amount: 1}
+    // ]
+    userCart = shallow(<Cart />)
   })
 
-  it('renders the email in an h3', () => {
-    expect(userCart.find('h3').text()).to.be.equal('Welcome, cody@email.com')
+  it('renders an h2 element', () => {
+    expect(userCart.find('h2').text()).to.be.equal('Your Cart')
   })
 })
