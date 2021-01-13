8a87d29f08fb8bb6321cd96eb3765893247e147d
Ali Wojcikiewicz <aliwoj@Alis-MBP.cable.rcn.com>
2021-01-13 10:36:07 -0600

    feat: redux sub-reducer for adding products to cart on front-end

---
+++ client/store/cart.js
@@ -0,0 +1,38 @@
+import Axios from 'axios'
+
+// action type
+const ADD_TO_CART = 'ADD_TO_CART'
+
+// action creator
+export const addToCart = (product, amount) => {
+  return {
+    type: ADD_TO_CART,
+    product,
+    amount
+  }
+}
+
+// thunks
+export const addToUserCart = (user, product, amount) => {
+  return async dispatch => {
+    try {
+      //use route to add products to user's persistent cart in the database
+      dispatch(addToCart(product, amount))
+    } catch (err) {
+      console.log('error in addToUserCart thunk\n', err)
+    }
+  }
+}
+
+// initial state
+const initialState = []
+
+// reducer
+export default function allProductsReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return [...state, {product: action.product, amount: action.amount}]
+    default:
+      return state
+  }
+}
