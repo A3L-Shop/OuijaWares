8679faacdb0bb580090eed6280f8ce18cf2e3f13
Annie Cho <gjhwa11@gmail.com>
2021-01-13 22:18:35 -0500

    attempt to fix sequelize heroku deploy problem

--- server/db/db.js
+++ server/db/db.js
@@ -2,13 +2,22 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+let db
+if (process.env.DATABASE_URL) {
+  db = new Sequelize(process.env.DATABASE_URL, {
+    dialet: 'postgres',
+    protocol: 'postgres',
+    logging: true
+  })
+} else {
+  db = new Sequelize(
+    process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+    {
+      logging: false
+    }
+  )
+}
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
-    logging: false
-  }
-)
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
