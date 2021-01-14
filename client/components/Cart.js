a0a8b0e29de2a3059808b0117fb4f83e72efd72f
anahit <sathytrench@gmail.com>
2021-01-14 16:43:19 -0500

    feature: began adding edit cart features to Cart component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,27 +2,45 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
 export class Cart extends Component {
+  constructor() {
+    super()
+    this.state = {
+      totalPrice: 0
+      //product: {productObject}, second key is quantity
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  async handleChange(id) {}
+
   render() {
-    let totalPrice = 0
     return (
       <div>
         <h2>Your Cart</h2>
         {this.props.cartItems.length ? (
           this.props.cartItems.map(item => {
             const product = item.product
-            const amount = item.amount
-            totalPrice += product.price * amount
+            const quantity = item.quantity
+            this.totalPrice += product.price * quantity
             return (
               <div key={product.id}>
                 <h4>{product.name}</h4>
-                <h4>{amount}</h4>
+                <input
+                  type="number"
+                  id="quantity"
+                  name="quantity"
+                  min="0"
+                  max="10"
+                  value={quantity}
+                  onChange={() => this.handleChange(product.id)}
+                />
               </div>
             )
           })
         ) : (
           <h4>Your cart is empty.</h4>
         )}
-        <h3>{`Total Price: $${totalPrice ? totalPrice : '0.00'}`}</h3>
+        <h3>{`Total Price: $${this.totalPrice ? this.totalPrice : '0.00'}`}</h3>
       </div>
     )
   }
