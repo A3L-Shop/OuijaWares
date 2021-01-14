ef7cf059dc179d55e0120ca9fcabde207610cd6f
Annie Cho <gjhwa11@gmail.com>
2021-01-14 11:32:39 -0500

    db/db.js process.env.database_url set up change back to the way it was

--- server/db/db.js
+++ server/db/db.js
@@ -2,19 +2,13 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-let db
-if (process.env.DATABASE_URL) {
-  db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    logging: true,
-    ssl: true
-  })
-} else {
-  db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
+
+const db = new Sequelize(
+  (process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
     logging: false
   })
-}
+)
 
 module.exports = db
 
