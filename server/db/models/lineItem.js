d9a4ee77a16f1b2e278af093d0d3eb74062f2c01
Ali Wojcikiewicz <aliwoj@Alis-MBP.cable.rcn.com>
2021-01-14 12:02:51 -0600

    chore: removed timestamps from order, product, and lineItem tables

--- server/db/models/lineItem.js
+++ server/db/models/lineItem.js
@@ -1,13 +1,17 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const LineItem = db.define('line-item', {
-  quantity: {
-    type: Sequelize.INTEGER,
-    validate: {
-      min: 1
+const LineItem = db.define(
+  'line-item',
+  {
+    quantity: {
+      type: Sequelize.INTEGER,
+      validate: {
+        min: 1
+      }
     }
-  }
-})
+  },
+  {timestamps: false}
+)
 
 module.exports = LineItem
