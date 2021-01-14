e0688fd8fafa3a3b1c1f8bcf1bd80d4efa00d7ec
Annie Cho <gjhwa11@gmail.com>
2021-01-14 11:40:06 -0500

    db/db.js reverting changes back to the separated version of production and development

--- server/db/db.js
+++ server/db/db.js
@@ -2,14 +2,19 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-
-const db = new Sequelize(
-  (process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
-    logging: false,
+let db
+if (process.env.DATABASE_URL) {
+  db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: true,
     ssl: true
   })
-)
+} else {
+  db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
+    logging: false
+  })
+}
 
 module.exports = db
 
