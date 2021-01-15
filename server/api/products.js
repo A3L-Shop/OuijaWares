a848164a3f8cece668c75d597eb0bd3e8a05e2fe
Annie Cho <gjhwa11@gmail.com>
2021-01-14 19:22:26 -0500

    api/products: adds admin gate and adds POST, PUT, DELETE routes for a single product

--- server/api/products.js
+++ server/api/products.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 const {Product} = require('../db/models')
 module.exports = router
 
+const isAdmin = (req, res, next) =>
+  req.user.isAdmin ? next() : res.send('None shall pass')
+
 router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll({
@@ -26,5 +29,43 @@ router.get('/:productId', async (req, res, next) => {
     res.json(product)
   } catch (error) {
     next(error)
+  }
+})
+
+router.post('/', isAdmin, async (req, res, next) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      imageUrl,
+      inventoryAmount,
+      category
+    } = req.body
+    const product = await Product.findOrCreate({
+      where: {name, description, price, imageUrl, inventoryAmount, category}
+    })
+    res.send(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:id', isAdmin, async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id)
+    product.update(req.body)
+    res.sendStatus(204)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:id', isAdmin, async (req, res, next) => {
+  try {
+    await Product.destroy({where: {id: req.params.id}})
+    res.status(204).end()
+  } catch (err) {
+    next(err)
   }
 })
